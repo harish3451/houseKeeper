cb1fa3cfc0e4b91d0ff08a3a501dde3294a1863a
harish kumar <Harishkumar@gmail.com>
2023-08-07 15:45:49 +0530

    initial commit

---
+++ BackEnd/HouseKeeper_backend/src/main/java/com/HouseKeeper/HouseKeeperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.HouseKeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseKeeperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseKeeperBackendApplication.class, args);
+	}
+
+}
---
+++ BackEnd/HouseKeeper_backend/src/test/java/com/HouseKeeper/HouseKeeperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HouseKeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseKeeperBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
